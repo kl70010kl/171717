8fbe259c6ab9625d4a37617a7a5044a8a7e2b2bb
Jo_Muroi <jojojojo.muroi.12@docomo.ne.jp>
2019-07-20 15:01:46 +0900

    hpゲージ

--- Assets/Scripts/HpBarCtrl.cs
+++ Assets/Scripts/HpBarCtrl.cs
@@ -5,20 +5,46 @@ using UnityEngine.UI;
 
 public class HpBarCtrl : MonoBehaviour
 {
-    Slider _slider;
+    //Slider _slider;
     // Start is called before the first frame update
     void Start()
     {
-        _slider = GameObject.Find("Slider").GetComponent<Slider>();
+        //_slider = GameObject.Find("Slider").GetComponent<Slider>();
+
+        //float maxHp = 100f;
+        //float nowHp = 100f;
+
+        ////スライダーの最大値の設定
+        //_slider.maxValue = maxHp;
+
+        ////スライダーの現在値の設定
+        //_slider.value = nowHp;
     }
-    float _hp = 100;
-    
+
+    //public void TakeDamage(int amount)
+    //{
+    //    _slider -= amount;
+    //    if (_slider <= 0)
+    //    {
+    //        _slider = 0;
+    //        Debug.Log("Dead!");
+    //    }
+    //}
 
     // Update is called once per frame
     void Update()
     {
-        
-        _slider.value = _hp;
-            
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    _slider.value -= 10f;
+        //}
+
+        //_hp += 1.0f;
+        //if(_hp>_slider.maxvalue)
+        //{
+        //    _hp = _slider.minvalue;
+        //}
+        //_slider.value = _hp;
+
     }
 }
