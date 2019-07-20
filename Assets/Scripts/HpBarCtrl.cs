723ccdeb6f0b4b4fcf4cb443269fe789ad2bc766
kl70010kl <45251851+kl70010kl@users.noreply.github.com>
2019-07-20 15:44:56 +0900

    aaa

--- Assets/Scripts/HpBarCtrl.cs
+++ Assets/Scripts/HpBarCtrl.cs
@@ -5,46 +5,20 @@ using UnityEngine.UI;
 
 public class HpBarCtrl : MonoBehaviour
 {
-    //Slider _slider;
+    Slider _slider;
     // Start is called before the first frame update
     void Start()
     {
-        //_slider = GameObject.Find("Slider").GetComponent<Slider>();
-
-        //float maxHp = 100f;
-        //float nowHp = 100f;
-
-        ////スライダーの最大値の設定
-        //_slider.maxValue = maxHp;
-
-        ////スライダーの現在値の設定
-        //_slider.value = nowHp;
+        _slider = GameObject.Find("Slider").GetComponent<Slider>();
     }
-
-    //public void TakeDamage(int amount)
-    //{
-    //    _slider -= amount;
-    //    if (_slider <= 0)
-    //    {
-    //        _slider = 0;
-    //        Debug.Log("Dead!");
-    //    }
-    //}
+    float _hp = 100;
+    
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    _slider.value -= 10f;
-        //}
-
-        //_hp += 1.0f;
-        //if(_hp>_slider.maxvalue)
-        //{
-        //    _hp = _slider.minvalue;
-        //}
-        //_slider.value = _hp;
-
+        
+        _slider.value = _hp;
+            
     }
 }
