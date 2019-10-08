eafbda0d9febac36afda553a74bafde9795b0df1
reiiti <reiitininomae@gmail.com>
2019-10-08 10:57:24 +0900

    Droneの編集２

---
+++ Assets/DroneAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneAttack : MonoBehaviour
+{
+    //フィールド
+    public GameObject droneBullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BulletAttack()
+    {
+        Instantiate(droneBullet, gameObject.transform.position, gameObject.transform.rotation);
+    }
+}
